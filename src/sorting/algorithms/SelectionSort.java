d7f6ed17e4c63e9c39c93b5f4bd4ad4b3d3cb886
Дементьев Кирилл Владиславович <kvdementev@edu.hse.ru>
2025-03-29 00:21:42 +0300

    new class - SelectionSort

---
+++ src/sorting/algorithms/SelectionSort.java
@@ -0,0 +1,22 @@
+package sorting.algorithms;
+
+import sorting.SortAlgorithm;
+
+public class SelectionSort implements SortAlgorithm {
+    @Override
+    public void sort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                int temp = array[minIndex];
+                array[minIndex] = array[i];
+                array[i] = temp;
+            }
+        }
+    }
+}
