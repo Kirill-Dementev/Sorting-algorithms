c2f52f92f1d076a9c41571ae099bf01c2918ae7d
Дементьев Кирилл Владиславович <kvdementev@edu.hse.ru>
2025-03-29 01:13:50 +0300

    new class - InformationSort

---
+++ src/InformationSort.java
@@ -0,0 +1,29 @@
+public class InformationSort {
+    private final int comparisons;
+    private final int swaps;
+    private final long time;
+    private final long memory;
+
+    public InformationSort(int comparisons, int swaps, long time, long memory) {
+        this.comparisons = comparisons;
+        this.swaps = swaps;
+        this.time = time;
+        this.memory = memory;
+    }
+
+    public int getComparisons() {
+        return comparisons;
+    }
+
+    public int getSwaps() {
+        return swaps;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public long getMemory() {
+        return memory;
+    }
+}
