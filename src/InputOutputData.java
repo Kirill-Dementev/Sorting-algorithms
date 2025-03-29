129a5b3fdb4ac514bfffa6c18bd674344bb16f35
Дементьев Кирилл Владиславович <kvdementev@edu.hse.ru>
2025-03-29 18:18:06 +0300

    New function in the class InputOutputData

--- src/InputOutputData.java
+++ src/InputOutputData.java
@@ -1,5 +1,6 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import sorting.InformationSort;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -15,7 +16,7 @@ public class InputOutputData {
                     throw new InvalidDataException("Неверный тип данных в файле");
                 }
                 numbers.add(scanner.nextInt());
-                if (numbers.size() > 10000) {
+                if (numbers.size() > 100000) {
                     throw new InvalidDataException("В файле содержиться больше 10000 чисел");
                 }
             }
@@ -23,5 +24,24 @@ public class InputOutputData {
             throw new InvalidDataException("Не найден входной файл по пути " + inputFile);
         }
         return numbers.stream().mapToInt(i -> i).toArray();
+    }
+
+    public static void CreateOutputFile(InformationSort[] result, String outputFile, String[] nums) throws FileNotFoundException {
+        try (PrintWriter writer = new PrintWriter(outputFile)) {
+            String[] algorithms = {"Пузырьковая сортировка (BubbleSort)",
+                    "Быстрая сортировка (QuickSort)",
+                    "Сортировка слиянием (MergeSort)",
+                    "Сортировка вставками (InsertionSort)",
+                    "Сортировка выбором (SelectionSort)"
+            };
+            for (int i = 0; i < result.length; i++) {
+                writer.println(algorithms[Integer.parseInt(nums[i]) - 1] + ": ");
+                writer.println("Время выполнения - " + ((double) result[i].getTime() / 1000000000) + " секунд");
+                writer.println("Затраченная память - " + (double) result[i].getMemory() / 1024 + " Кб");
+                writer.println("Количество сравнений - " + result[i].getComparisons());
+                writer.println("Количество перемещений - " + result[i].getSwaps());
+                writer.println();
+            }
+        }
     }
 }
