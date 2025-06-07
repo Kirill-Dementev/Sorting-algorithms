84983b3edc2dcc807f2f206cc35ed8cc869d2fc7
Дементьев Кирилл Владиславович <kvdementev@edu.hse.ru>
2025-06-08 00:39:57 +0300

    new class - GnomeSort

---
+++ src/sorting/algorithms/GnomeSort.java
@@ -0,0 +1,41 @@
+package sorting.algorithms;
+
+import sorting.InformationSort;
+import sorting.SortAlgorithm;
+
+public class GnomeSort implements SortAlgorithm {
+    private long comparisons = 0;
+    private long swaps = 0;
+
+    @Override
+    public InformationSort sort(int[] array) {
+        long startTime;
+        long endTime;
+        long memory;
+        startTime = System.nanoTime();
+        Runtime runtime = Runtime.getRuntime();
+        long initialMemory = runtime.totalMemory() - runtime.freeMemory();
+
+        int index = 0;
+        while (index < array.length) {
+            comparisons++;
+            if (index == 0 || array[index] >= array[index - 1]) {
+                index++;
+            } else {
+                swap(array, index, index - 1);
+                index--;
+            }
+        }
+
+        endTime = System.nanoTime();
+        memory = (runtime.totalMemory() - runtime.freeMemory()) - initialMemory;
+        return new InformationSort(comparisons, swaps, endTime - startTime, memory);
+    }
+
+    private void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        swaps++;
+    }
+}
